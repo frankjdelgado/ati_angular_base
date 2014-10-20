15d1ffd6752fca2ef2030aa74e624373a63e573f
Frank <frankjdelgado@gmail.com>
2014-10-20 12:12:56 -0430

    controllers and show inside a loop

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var atiApp = angular.module('atiApp',	['ngRoute']);
 
 // app config for routes
 atiApp.config(function($routeProvider){
+
 	// url and config
 	// define endpoint and file
 	// define controler
@@ -21,4 +22,26 @@ atiApp.config(function($routeProvider){
 			templateUrl: 'pages/user.html',
 			controller: 'userController'
 		});
-	});
+});
+
+// Config controllers
+// specify controller name and function
+atiApp.controller('mainController',function($scope){
+
+	// Variables inside scope to show. Messages, database records, etc.
+	$scope.message = 'Hello World!';
+
+});
+
+atiApp.controller('packageController',function($scope){
+
+	$scope.packages = [
+			{name: 'package 1'},
+			{name: 'package 2'},
+			{name: 'package 3'},
+			{name: 'package 4'},
+			{name: 'package 5'},
+			{name: 'package 6'},
+		];
+
+});
